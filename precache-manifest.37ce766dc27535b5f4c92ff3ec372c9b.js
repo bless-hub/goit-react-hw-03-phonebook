c15df02fde9d7d40826e650f2e056a5e1f5cba16
Vladyslav Bondarenko <bvbasys@gmail.com>
2020-09-27 15:42:21 +0300

    Updates

---
+++ precache-manifest.37ce766dc27535b5f4c92ff3ec372c9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5df15baae2143a3f9a64120065782b91",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "54cd1ce2c20ac7a13b6d",
+    "url": "/goit-react-hw-03-phonebook/static/css/main.187ff22a.chunk.css"
+  },
+  {
+    "revision": "136a304fa6803b15355a",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.2593a25f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.2593a25f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54cd1ce2c20ac7a13b6d",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.dafc5832.chunk.js"
+  },
+  {
+    "revision": "a8921e93a59bc20c1792",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.739edc23.js"
+  }
+]);
